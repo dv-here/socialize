e242c7e3d524babfec5b6d4d2a959728fca82e38
dv-here <divyanshu.rai8@gmail.com>
2020-09-05 12:24:12 +0530

    added home page,form to add post, ui improved

--- app.js
+++ app.js
@@ -15,19 +15,36 @@ app.use(express.static(path.join(__dirname, "public")));
 app.set('views',path.join(__dirname,"views"));
 app.set('view engine','ejs');
 
+let posts = [
+    {image:"https://images.unsplash.com/photo-1469854523086-cc02fe5d8800?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1008&q=80",caption:"Caption1",creator:"user1"},
+    {image:"https://images.unsplash.com/photo-1469854523086-cc02fe5d8800?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1008&q=80",caption:"Caption2",creator:"user2"},
+    {image:"https://images.unsplash.com/photo-1469854523086-cc02fe5d8800?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1008&q=80",caption:"Caption3",creator:"user3"}, 
+]
 
 
 app.get('/',(req,res)=>{
     res.send("hello there....");
 });
 
+app.get('/posts',(req,res)=>{
+    
+    res.render("home",{posts:posts});
+})
 
+app.post("/posts",(req,res)=>{
+    // get data from form
+    let image = req.body.image;
+    let caption = req.body.caption;
+    let newpost = {image:image,caption:caption,creator:"user"};
+    console.log(newpost);
+    posts.push(newpost);
+    // redirect to the posts page
+    res.redirect('/posts');
+});
 
-
-
-
-
-
+app.get('/post/new',(req,res)=>{
+    res.render("newPost");
+});
 
 
 
